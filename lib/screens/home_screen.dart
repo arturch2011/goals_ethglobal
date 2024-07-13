9d5c204e2003b8159284308c8cd8ef9628a0ca40
Artur Hashimoto <arturch2011@gmail.com>
2024-07-13 13:06:56 +0200

    home page + home widgets

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,239 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:goals_ethglobal/providers/user_info_provider.dart';
+import 'package:goals_ethglobal/screens/menu_screen.dart';
+import 'package:goals_ethglobal/utils/eth_utils.dart';
+import 'package:goals_ethglobal/widgets/home/carousel.dart';
+import 'package:goals_ethglobal/widgets/home/main_project.dart';
+import 'package:goals_ethglobal/widgets/home/profile_avatar.dart';
+import 'package:goals_ethglobal/widgets/home/project_card.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
+  Widget buildRecommendedExercises() {
+    return const Row(
+      children: [
+        Text(
+          'Você também pode gostar',
+          style: TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // const Spacer(),
+        // GestureDetector(
+        //   onTap: () => {},
+        //   child: const Text(
+        //     'Ver mais',
+        //     style: TextStyle(
+        //       fontSize: 17,
+        //       color: Color.fromRGBO(156, 156, 156, 1),
+        //       fontWeight: FontWeight.bold,
+        //     ),
+        //   ),
+        // ),
+      ],
+    );
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(ethUtilsProviders);
+    final ethUtils = ref.read(ethUtilsProviders.notifier);
+    ethUtils.getGoals();
+    ethUtils.getMyEnteredGoals();
+    ethUtils.getMyGoals();
+    List<dynamic> myGoals = [];
+    List<dynamic> goals = [];
+    List<dynamic> unstartedGoals = [];
+    List<dynamic> publicGoals = [];
+    String? completeName;
+    try {
+      completeName =
+          context.watch<UserProvider>().userInfos[0]['userInfo']["name"];
+    } catch (e) {
+      completeName = null;
+    }
+
+    final String nullName;
+    completeName == null ? nullName = "Usuário" : nullName = completeName;
+    List<String> names = nullName.split(" ");
+    final String firstName = names[0];
+
+    goals = ethUtils.goals;
+    myGoals = ethUtils.myEnteredGoals;
+    print("Listaaa  ${myGoals[0]}");
+    // print("homepage: ${goals[0][goals[0].length - 1]}");
+    int goalsLength;
+    int goalsLength2;
+    int lastGoalIndex = -1;
+
+    if (goals.isNotEmpty) {
+      final List<dynamic> goal = goals[0];
+      unstartedGoals = goal
+          .where((element) =>
+              (element[7].toInt() - DateTime.now().millisecondsSinceEpoch) /
+                  (1000 * 60 * 60 * 24) >
+              0)
+          .toList();
+      publicGoals =
+          unstartedGoals.where((element) => element[11] == true).toList();
+      if (publicGoals.isEmpty) {
+        goalsLength = -1;
+        goalsLength2 = -1;
+      } else if (publicGoals.length == 1) {
+        goalsLength = publicGoals[publicGoals.length - 1][0].toInt();
+        goalsLength2 = -1;
+      } else {
+        goalsLength = publicGoals[publicGoals.length - 1][0].toInt();
+        goalsLength2 = publicGoals[publicGoals.length - 2][0].toInt();
+      }
+    } else {
+      goalsLength = -1;
+      goalsLength2 = -1;
+    }
+
+    if (myGoals.isEmpty) {
+      lastGoalIndex = -1;
+    } else if (myGoals[0].isEmpty) {
+      lastGoalIndex = -1;
+    } else {
+      lastGoalIndex = myGoals[0][myGoals[0].length - 1].toInt();
+    }
+
+    // if (goals.isEmpty) {
+    //   goalsLength = -1;
+    //   goalsLength2 = -1;
+    // } else if (goals[0].length <= 1) {
+    //   goalsLength = goals[0].length - 1;
+    //   goalsLength2 = -1;
+    // } else {
+    //   goalsLength = goals[0].length - 1;
+    //   goalsLength2 = goals[0].length - 2;
+    // if (myGoals.isEmpty) {
+    //   lastGoalIndex = -1;
+    // } else if (myGoals[0].isEmpty) {
+    //   lastGoalIndex = -1;
+    // } else {
+    //   lastGoalIndex = myGoals[0][myGoals[0].length - 1].toInt();
+    // }
+    // }
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const BuildProfileAvatar(
+                  size: 30,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  'Olá, $firstName!',
+                  textHeightBehavior: const TextHeightBehavior(
+                      applyHeightToFirstAscent: true,
+                      applyHeightToLastDescent: false),
+                  style: Theme.of(context).textTheme.titleLarge,
+                  textAlign: TextAlign.center,
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const Menu();
+                        },
+                      ),
+                    );
+                  },
+                  icon: const Icon(
+                    Icons.menu,
+                    size: 30,
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Container(
+                      width: double.infinity,
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5), // Sombra
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                            // Ajuste a sombra vertical aqui
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          MainProjectInfo(
+                            index: lastGoalIndex,
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Padding(
+                    //   padding: const EdgeInsets.all(3),
+                    //   child: GestureDetector(
+                    //     onTap: () => {},
+                    //     child: const Row(
+                    //       mainAxisAlignment: MainAxisAlignment.end,
+                    //       children: [
+                    //         Text(
+                    //           'Ver todos os meus projetos',
+                    //           style: TextStyle(
+                    //             fontSize: 12,
+                    //           ),
+                    //         ),
+                    //         SizedBox(width: 5),
+                    //         Icon(
+                    //           Icons.arrow_forward_ios_rounded,
+                    //           size: 13,
+                    //         )
+                    //       ],
+                    //     ),
+                    //   ),
+                    // ),
+                    const SizedBox(height: 20),
+                    const CarouselWithIndicatorDemo(),
+                    const SizedBox(height: 20),
+                    buildRecommendedExercises(),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ProjectCard(index: goalsLength),
+                          const SizedBox(width: 10),
+                          ProjectCard(index: goalsLength2),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 100),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/home/project_card.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:goals_ethglobal/utils/eth_utils.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProjectCard extends StatefulWidget {
+  final int index;
+  const ProjectCard({super.key, required this.index});
+
+  @override
+  State<ProjectCard> createState() => _ProjectCardState();
+}
+
+class _ProjectCardState extends State<ProjectCard> {
+  @override
+  Widget build(BuildContext context) {
+    late int index;
+
+    return Consumer(
+      builder: (context, ref, child) {
+        ref.watch(ethUtilsProviders);
+        final ethUtils = ref.watch(ethUtilsProviders.notifier);
+        // late final List<dynamic> goal;
+        // late final String title;
+        // late final BigInt startTime;
+
+        String title = 'title';
+        String imgUrl = '1111111111111111111111111111111';
+        int startTime = 0;
+        double timer = 0;
+        int vezes = 1;
+        int metaU = 1;
+        int meta = 0;
+        String metaType = '--';
+        String frequency = 'Diária';
+        String fname = 'dia';
+        List<dynamic> goal = [];
+
+        if (ethUtils.goals.isEmpty) {
+          index = -1;
+        } else if (ethUtils.goals[0].isEmpty) {
+          index = -1;
+        } else {
+          index = widget.index;
+          if (index != -1) {
+            final goal = ethUtils.goals[0][index];
+            title = goal[1];
+            imgUrl = goal[17][0];
+            startTime = goal[7].toInt();
+            timer = (startTime - DateTime.now().millisecondsSinceEpoch) /
+                (1000 * 60 * 60 * 24);
+            vezes = goal[19].toInt();
+            metaU = goal[20].toInt();
+            meta = vezes * metaU;
+            metaType = goal[18];
+            frequency = goal[4];
+
+            if (frequency == 'Diária') {
+              fname = 'dia';
+            } else if (frequency == 'Semanal') {
+              fname = 'semana';
+            } else if (frequency == 'Mensal') {
+              fname = 'mês';
+            } else {
+              fname = '-';
+            }
+          }
+        }
+
+        return Expanded(
+          child: GestureDetector(
+            onTap: () {
+              // Navigator.of(context).push(
+              //   MaterialPageRoute(
+              //     builder: (context) {
+              //       return ProjectDetails(index: index);
+              //     },
+              //   ),
+              // );
+            },
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(20),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5), // Sombra
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    // Ajuste a sombra vertical aqui
+                  ),
+                ],
+              ),
+              child: (index == -1)
+                  ? const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8, vertical: 20),
+                      child: Text('Nenhum projeto'),
+                    )
+                  : Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20),
+                          ),
+                          child: imgUrl.substring(0, 28) ==
+                                  'https://gateway.pinata.cloud'
+                              ? Image.network(imgUrl,
+                                  fit: BoxFit.cover,
+                                  height: 150,
+                                  width: double.infinity)
+                              : Image.asset('assets/images/splash_image2.png',
+                                  fit: BoxFit.cover,
+                                  height: 150,
+                                  width: double.infinity),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8, vertical: 8),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      title,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Icon(Icons.access_time_rounded,
+                                          size: 15),
+                                      Text(
+                                        '${timer.toStringAsFixed(0)}d',
+                                        style: const TextStyle(
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                '$meta $metaType por $fname', // Valor do progresso (substitua pelo valor real)
+                                style: const TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                width: double.infinity,
+                                child: TextButton(
+                                  onPressed: () {
+                                    // Navigator.of(context).push(
+                                    //   MaterialPageRoute(
+                                    //     builder: (context) {
+                                    //       return ProjectDetails(index: index);
+                                    //     },
+                                    //   ),
+                                    // );
+                                  },
+                                  style: TextButton.styleFrom(
+                                    backgroundColor:
+                                        Theme.of(context).primaryColor,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    "Participar",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
